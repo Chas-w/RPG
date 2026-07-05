ba91de9e03bf1f57cd4856d0d85620d3c930f082
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-07-05 15:00:01 -0400

    commented script

--- Scripts/lock_picking.gd
+++ Scripts/lock_picking.gd
@@ -7,7 +7,7 @@ extends Control
 @export_category("Pick Zone")
 @export var limit : Node2D
 ##how easy to get in range
-@export_range(2,20,.5) var safety : float
+@export_range(.5,20,.5) var safety : float
 @export var pick_limits : Vector2
 @export var pick_tip : Node2D
 @export var limit_tip : Node2D
@@ -24,7 +24,7 @@ var solution_value : float = 90
 var pick_time_limit_max : float = 2
 var pick_time_limit :float
 var break_pick : bool
-var break_buffer_max : float = .5
+@export_range(.1,2,.1) var break_buffer_max : float = .5
 var break_buffer 
 
 var solve_speed_max = 5
@@ -32,13 +32,14 @@ var solve_speed_min = .1
 var solve_speed = 1
 var solving : bool
 var reset : bool 
+var solved : bool 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready(): 
+	##setup
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#set a random rotation for the solution
 	var new_rot = (randf_range(deg_to_rad(pick_limits.x), deg_to_rad(pick_limits.y)))
 	limit.rotation = new_rot
-	limit.scale.y = randf_range(1,safety)
 	break_buffer = break_buffer_max
 	pick_time_limit = pick_time_limit_max
 
@@ -48,54 +49,76 @@ func _process(delta):
 	if (break_pick):
 		_handle_break_pick()
 
+##handles the behavior while the player attempts to solve
 func _handle_solver():
-	if (Input.is_action_pressed("interact") && pick_amount > 0):
+	if (Input.is_action_just_pressed("interact")):
 		solving = true
+	if (Input.is_action_pressed("interact") && pick_amount > 0): #if the player has sufficient amount of picks
+		##if the pick is within solvable range
 		if(_get_angle(pivot.global_position,pick_tip.global_position, limit_tip.global_position) <= 90 + safety && _get_angle(pivot.global_position,pick_tip.global_position, limit_tip.global_position) >= 90 - safety):
 			if (lock_background.rotation > deg_to_rad(-90)):
 				lock_background.rotation -=  get_process_delta_time() * solve_speed
+			else:
+				lock_background.rotation = deg_to_rad(-90)
+			solved = true
+			#success
 		else:
+			##variable to represent how long the lock will solve for before failing
 			var time_modifier = (90 - _get_angle(pivot.global_position,pick_tip.global_position, limit_tip.global_position))/3
-			if (pick_time_limit > 0):
+			if (pick_time_limit > 0): #solve
 				pick_time_limit -= get_process_delta_time() * time_modifier
 				if (lock_background.rotation > deg_to_rad(-90)):
 					lock_background.rotation -=  get_process_delta_time() * solve_speed
-			else:
+				else:
+					lock_background.rotation = deg_to_rad(-90)
+			else: #when time is up start break sequence
 				break_pick = true
-	else:
+	else: #when the player releases input reset 
 		solving = false
 		if (!break_pick):
-			if(lock_background.rotation < 0):
+			if(lock_background.rotation < deg_to_rad(0)):
 				lock_background.rotation +=  get_process_delta_time() * 3
+			else:
+				lock_background.rotation = deg_to_rad(0)
 			if(pick_time_limit != pick_time_limit_max):
 				pick_time_limit = pick_time_limit_max
 
+##handles behavior after a failed attempt
 func _handle_break_pick():
-	if(solving):
-		if(break_buffer > 0):
+	if(solving): #if the player continues to try and solve
+		if(break_buffer > 0): #give a buffer
 			break_buffer -= get_process_delta_time()
-		else:
-			if(lock_background.rotation < 0):
+		else: #once the time is up reset the lock
+			if(lock_background.rotation < deg_to_rad(0)):
 				lock_background.rotation +=  get_process_delta_time() 
-			else:
+			else: #remove a pick and reset
 				break_pick = false
 				break_buffer = break_buffer_max
 				pick_amount -= 1
-				print(pick_amount)
-	else:
+				print("PICK BREAK")
+				solving = false
+				#break animation
+	else: #otherwise reset without removing a pick
 		break_pick = false
 		break_buffer = break_buffer_max
-		#break animation
 
-	pass
-
-func _handle_pick_rotation():
+##handles making the picker follow mouse movement
+func _handle_pick_rotation(): 
 	if (move_pick && !solving && !break_pick):
 		if(higher):
 			pick.rotation += deg_to_rad(pick_speed * mouse_speed * get_process_delta_time())
 		else:
 			pick.rotation -= deg_to_rad(pick_speed * mouse_speed * get_process_delta_time())
 	move_pick = false
+
+##returns how close to the solution the picker is
+func _get_angle(vec1 : Vector2, vec2 : Vector2, vec3 : Vector2):
+	var length_a = sqrt(pow(vec2.x - vec1.x, 2) + pow(vec2.y - vec1.y,2))
+	var length_b = sqrt(pow(vec3.x - vec2.x,2) + pow(vec3.y - vec2.y, 2))
+	var length_c = sqrt(pow(vec3.x - vec1.x,2) + pow(vec3.y - vec1.y, 2))
+	
+	var calculation = ((length_a * length_a) + (length_b * length_b) - (length_c * length_c)) / (2 * length_a * length_b)
+	return rad_to_deg(acos(calculation)) 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -105,11 +128,3 @@ func _input(event: InputEvent) -> void:
 			higher = true
 		else:
 			higher = false
-
-func _get_angle(vec1 : Vector2, vec2 : Vector2, vec3 : Vector2):
-	var length_a = sqrt(pow(vec2.x - vec1.x, 2) + pow(vec2.y - vec1.y,2))
-	var length_b = sqrt(pow(vec3.x - vec2.x,2) + pow(vec3.y - vec2.y, 2))
-	var length_c = sqrt(pow(vec3.x - vec1.x,2) + pow(vec3.y - vec1.y, 2))
-	
-	var calculation = ((length_a * length_a) + (length_b * length_b) - (length_c * length_c)) / (2 * length_a * length_b)
-	return rad_to_deg(acos(calculation)) 
