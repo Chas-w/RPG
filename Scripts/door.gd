91444f62b124208e3340c9fcd4a92b77476b6628
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-07-13 17:48:37 -0400

    more door

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,6 +1,23 @@
 extends Node3D
 @export var locked : bool
 @export var lock_picking : Control
+var player
 
 func _ready():
-	lock_picking.visible = false
+	if (!locked):
+		lock_picking.queue_free() #if the door isn't locked get rid of this node
+	else:
+		lock_picking.visible = false #hide this node
+	
+
+func _process(delta):
+	if (player != null):
+		print(player)
+
+func _on_open_trigger_body_entered(body):
+	if(body.is_in_group("Player")):
+		player = body.get_parent()
+
+
+func _on_open_trigger_body_exited(body):
+	player = null # Replace with function body.
